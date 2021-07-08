bf401bfa87a9d06ba27aa11cb58f5ce333660e42
robertordz14 <hroberto.rdz@gmail.com>
2021-07-08 16:31:24 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import React, {useState} from 'react'
+import QrScan from 'react-qr-reader'
+
+function App() {
+
+    const [qrscan, setQrscan] = useState('Ningun QR escaneado');
+    const handleScan = data => {
+        if (data) {
+            setQrscan(data)
+            alerta()
+        }
+    }
+    const handleError = err => {
+    console.error(err)
+    }
+  
+    return (
+      <div className="escaneador">
+            <h1>Escanea el código QR</h1>
+              <div className="lector">
+                <QrScan 
+                    delay={300}
+                    onError={handleError}
+                    onScan={handleScan}
+                    style={{ height: 240, width: 320 }}
+                />
+                
+              </div>
+
+            {/* <div>
+             <Button href={qrscan}>Ir a la pagina</Button>            
+            </div> */}
+            
+      </div>
+    );
+
+    function alerta(){
+      var otros = window.confirm("Codigo QR escaneado con exito, ¿desea abrir la página?");
+      if (otros === true) {
+        window.open(qrscan)
+       } else {
+         window.alert("Escaneo cancelado")
+      }
+    }
+  }
+  
+  export default App;
+  
